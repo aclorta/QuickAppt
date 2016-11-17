92c33762e4d0ee5752b49e17d29dad19560a35d8
cmchristi <ctmcknig@uci.edu>
2016-11-17 13:39:18 -0800

    Added Physician Activity

---
+++ app/src/main/java/com/example/team6/quickappt/Patient_Profile.java
@@ -0,0 +1,13 @@
+package com.example.team6.quickappt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Patient_Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient__profile);
+    }
+}
