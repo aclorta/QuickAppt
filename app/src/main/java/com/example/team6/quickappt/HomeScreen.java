c5fc975957a58d8001d1ec92b33cad494369f017
aclorta <aclorta@gmail.com>
2016-11-22 09:47:43 -0800

    updated search button link

--- app/src/main/java/com/example/team6/quickappt/HomeScreen.java
+++ app/src/main/java/com/example/team6/quickappt/HomeScreen.java
@@ -78,7 +78,7 @@ public class HomeScreen extends AppCompatActivity {
         btnClick.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HomeScreen.this, SearchActivity.class);
+                Intent intent = new Intent(HomeScreen.this, SearchAct.class);
                 intent.putExtra("patientID", patientID);
                 startActivity(intent);
             }
